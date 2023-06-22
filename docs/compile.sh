4b22003a7ee9e7355bdb9923a7d41e12979027b3
alexoz12v2 <oboken1974@hotmail.it>
2023-06-22 23:47:42 +0200

    tesi colorimetria

--- docs/compile.sh
+++ docs/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 mkdir intermediate; 
 
-lualatex --output-directory=./intermediate main.tex \
+lualatex --shell-escape --output-directory=./intermediate main.tex \
 && biber ./intermediate/main \
 && makeglossaries -d ./intermediate main \
-&& lualatex --output-directory=./intermediate main.tex \
+&& lualatex --shell-escape --output-directory=./intermediate main.tex \
 && mv ./intermediate/main.pdf ./main.pdf;
 
 evince ./main.pdf;
